ff5a7e18b2e8314c2b2e3aa9c20aa72e61c35379
JonSeven <mazahaker5555@yandex.ru>
2023-06-06 22:38:27 +0300

    feat: add model Review & Controller Reviews

---
+++ src/main/java/ru/yandex/practicum/filmorate/model/Review.java
@@ -0,0 +1,31 @@
+package ru.yandex.practicum.filmorate.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+
+@AllArgsConstructor
+@Data
+public class Review {
+    private final long reviewId;
+
+    @NotNull
+    @NotBlank(message = "content can't be blank")
+    private final String content;
+
+    @NotNull
+    private final boolean isPositive;
+
+    @NotNull
+    @Positive
+    private final long userId;
+
+    @NotNull
+    @Positive
+    private final long filmId;
+
+    private final long useful;
+}
