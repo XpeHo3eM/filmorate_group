7008a496b50af1a8be824a3fcb8223547aa37f54
Da4nie-persick <darya.tcheltygmasheva@yandex.ru>
2023-06-09 17:50:21 +0700

    feat: добавить функциональность add-common-films

--- src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
@@ -1,6 +1,5 @@
 package ru.yandex.practicum.filmorate.storage;
 
-import org.springframework.transaction.annotation.Transactional;
 import ru.yandex.practicum.filmorate.model.Film;
 
 import java.util.List;
