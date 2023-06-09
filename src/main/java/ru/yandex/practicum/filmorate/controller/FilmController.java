dac8e86df76bd9ee5aec8bf5edb068a64eeac227
Da4nie-persick <darya.tcheltygmasheva@yandex.ru>
2023-06-09 16:34:13 +0700

    style: доработать общие фильмы

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -65,9 +65,4 @@ public class FilmController {
     public List<Film> getDirectorFilms(@PathVariable long directorId, @RequestParam String sortBy) {
         return service.getDirectorFilms(directorId, sortBy);
     }
-
-    @GetMapping("/common")
-    public List<Film> commonAndPopularFilm(@RequestParam long userId, @RequestParam long friendId) {
-        return service.commonAndPopularFilm(userId, friendId);
-    }
 }
